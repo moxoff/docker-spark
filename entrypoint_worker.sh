d3362fa04f1bfa6b729feb43efff96bd21fd0337
Stefano Brambilla <stefano.brambilla@moxoff.com>
2021-03-15 18:27:27 +0100

    UPDATE(docker): add aws jars; fix entrypoint for spark configs

--- entrypoint_worker.sh
+++ entrypoint_worker.sh
@@ -1,3 +1,16 @@
-printf "spark.worker.cleanup.enabled\t true\nspark.worker.cleanup.interval\t 43200\nspark.worker.cleanup.appDataTtl\t 1209600\n" >> /opt/spark/${SPARK_UNTAR_FOLDER}/conf/spark-defaults.conf
-printf "SPARK_WORKER_CORES=${SPARK_WORKER_CORES}\nSPARK_WORKER_MEMORY=${SPARK_WORKER_MEMORY}\n" >> /opt/spark/${SPARK_UNTAR_FOLDER}/conf/spark-env.sh
+# Define the parameters in the file spark-default.conf
+SPARK_DEFAULT_CLEANUP="spark.worker.cleanup.enabled\t true\nspark.worker.cleanup.interval\t 43200\nspark.worker.cleanup.appDataTtl\t 1209600\n"
+SPARK_DEFAULT_LOG="spark.local.dir\t /spark-local-dir\nspark.eventLog.dir\t /data/spark-events\nspark.history.fs.logDirectory\t /data/spark-events\nspark.eventLog.enabled\t true\n"
+SPARK_DEFAULT_REST_PORT="spark.master.rest.enabled\t true\n"
+
+SPARK_DEFAULT=${SPARK_DEFAULT_CLEANUP}+${SPARK_DEFAULT_LOG}+${SPARK_DEFAULT_REST_PORT}
+printf "${SPARK_DEFAULT}" > /opt/spark/${SPARK_UNTAR_FOLDER}/conf/spark-defaults.conf
+
+# Define the parameters in the file spark-env.conf
+SPARK_ENV_RESOURCES="SPARK_WORKER_CORES=${SPARK_WORKER_CORES}\nSPARK_WORKER_MEMORY=${SPARK_WORKER_MEMORY}\n"
+
+SPARK_ENV=${SPARK_ENV_RESOURCES}
+printf "${SPARK_ENV}" >> /opt/spark/${SPARK_UNTAR_FOLDER}/conf/spark-env.sh
+
+# Launch worker on spark master
 "/opt/spark/${SPARK_UNTAR_FOLDER}/bin/spark-class" org.apache.spark.deploy.worker.Worker ${SPARK_MASTER}
